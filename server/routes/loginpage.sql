ebab702c7a24619044994f5fe37baff5e87cedd2
FEGSRGe232ok <53444505+FEGSRGe232ok@users.noreply.github.com>
2020-04-19 15:06:27 +0100

    Add files via upload

---
+++ server/routes/loginpage.sql
@@ -0,0 +1,8 @@
+﻿CREATE DATABASE loginform;
+CREATE TABLE login_table (
+
+ID int PRIMARY KEY NOT NULL,
+EmailAddress1 varchar(300) NOT NULL,
+EmailPassword1 varchar (300) NOT NULL,
+
+);
---
+++ server/routes/signuppage.sql
@@ -0,0 +1,11 @@
+﻿CREATE DATABASE SIGNUP;
+CREATE TABLE signuptable (
+
+ID int PRIMARY KEY NOT NULL,
+FullName char(300) NOT NULL,
+EmailAddress2 varchar(300) NOT NULL,
+EmailPassword2 varchar (300) NOT NULL,
+PhoneNumber int(300) NOT NULL, 
+PostCode varchar(300) NOT NULL,
+
+);
