f06848802404a7e48c03a0a679f41605a0aeca93
hudlerr <ha241@student.le.ac.uk>
2020-04-21 16:33:19 +0100

    added logic for postcode api

--- server/routes/handler.js
+++ server/routes/handler.js
@@ -1,21 +1,45 @@
 const express = require('express');
 const path = require('path');
+const fetch = require('node-fetch');
 const { Router } = express; //Here we destructure (ES6) the Router value off of express
 const router = Router();
 const queries = require('./queries');
 
-// =========================================================
-// Routes
-// =========================================================
+let postcode; //global postcode var
+
+//Handles route for dashboard
+router.get('/my-dashboard', function(request, response) {
+    response.sendFile(path.join(__dirname, '..', '..', 'public', 'dashboard.html'));
+});
+
+//Helper method - handles get user neighbourhood request
+router.get('/neighbourhood', function(request, response) {
+    console.log("From /neighbourhood req");
+    var postcodeAPIURL = "https://api.postcodes.io/postcodes/" + postcode;
+    fetch(postcodeAPIURL)
+        .then(response => response.json())
+        .then(data => {
+            //data = JSON.parse(JSON.stringify(data));
+            console.log("passed on " + data.result['admin_ward']);
+            response.json({ ward: data.result['admin_ward'] });
+        })
+        .catch(error => console.log('error:', error));
+});
+
+//Handles post form in dashboard.js
+router.post('/dashboard', function(request, response) {
+    console.log("From post req" + request.body.postcode);
+    postcode = request.body.postcode
+    if (postcode.length < 5) { //TODO: This should be done client side
+        response.redirect('/error');
+    } else {
+        response.redirect('/my-dashboard');
+    }
+});
 
 //Handles the route for the home path
 router.get('/', function(request, response) {
     response.sendFile(path.join(__dirname, '..', '..', 'public', 'index.html'));
-});
-
-//Handles the route for the dashboard path
-router.get('/my-dashboard', function(request, response) {
-    response.sendFile(path.join(__dirname, '..', '..', 'public', 'dashboard.html'));
 });
 
 //Handles the route for the signup path
@@ -27,6 +51,12 @@ router.get('/create-user', function(request, response) {
 router.get('/login', function(request, response) {
 
     response.sendFile(path.join(__dirname, '..', '..', 'public', 'login.html'));
+});
+
+//Handles the route for the error path
+router.get('/error', function(request, response) {
+    response.writeHead(404, { 'content-type': 'text/html' });
+    response.end('<h1>Ooops somethings broken ...</h1>');
 });
 
 module.exports = router;
--- server/server.js
+++ server/server.js
@@ -7,7 +7,8 @@ const app = express();
 
 // Parses the request payload (if JSON) before the request goes to the handler
 app.use(bodyParser.json());
-
+app.use(bodyParser.urlencoded({ extended: false }));
+app.disable('x-powered-by');
 app.use(
     express.static(path.join(__dirname, '..', 'public'), { maxAge: '30d' })
 );
