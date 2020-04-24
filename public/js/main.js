0a82d5c1b324bc56758fd4e3631488affc39048d
Pearl-web <56562067+Pearl-web@users.noreply.github.com>
2020-04-24 16:24:57 +0100

    Update main.js
    removing my js from main js

--- public/js/main.js
+++ public/js/main.js
@@ -24,23 +24,3 @@ function updateDom(err, data) {
 }
 
 request('/neighbourhood', updateDom);
-
-
-
-/* validation for user post form  */
-function validateForm() {
-  var errormessage = "";
-
-   if (document.getElementById('task').value == "") {
-     errormessage += "Please enter a task.\n";
-   }
-
-   if (document.getElementById('description').value == "") {
-     errormessage += "Please enter a description of your task.\n";
-   }
-
-   if (errormessage != "") {
-     alert(errormessage);
-     return false;
-   }
-}
